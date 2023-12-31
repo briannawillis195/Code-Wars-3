cd522787b8e60c0549d3467505424c7f3ab22df8
brianna willis <brianna.willis195@gmail.com>
2023-12-31 12:00:00 -0500

    daily commit

---
+++ addItem.js
@@ -0,0 +1,6 @@
+//Add the value "codewars" to the websites array. After your code executes the websites array should == ["codewars"]
+
+// add the value "codewars" to the already defined websites array
+
+var websites = []
+websites.push('codewars')
