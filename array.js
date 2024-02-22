14b6eb47b3ff04f45d664aff1c05805ffb31af32
Brianna Willis <brianna.willis195@gmail.com>
2024-02-22 10:03:47 -0500

    daily commit

---
+++ array.js
@@ -0,0 +1,34 @@
+//    1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
+
+
+function getLength(arr){
+    //return length of arr
+    return arr.length
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    const first = arr[0]
+    return first
+  }
+  function getLast(arr){
+    //return the last element of arr
+    let last = arr[arr.length - 1]
+    return last
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    arr.push(el)
+    return arr 
+  }
+  function popElement(arr){
+    //pop an element from arr
+    
+    arr.pop()
+    return arr
+  }
