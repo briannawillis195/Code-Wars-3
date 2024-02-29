676b13ea8d290e5054f06ce69be69983faa5643f
Brianna Willis <brianna.willis195@gmail.com>
2024-02-29 10:23:16 -0500

    daily commit

---
+++ forIn.js
@@ -0,0 +1,16 @@
+//Create an array (which you will eventually return). Then, traverse obj, checking each key and value. If the length of the key is equal to 5, then push the key to your array. Separately, if the length of the value is equal to 5, then push the value to your array.
+
+function giveMeFive(obj) {
+  var result = [];
+
+  for (var key in obj) {
+    if (key.length === 5) {
+      result.push(key);
+    }
+    if (obj[key].length === 5) {
+      result.push(obj[key]);
+    }
+  }
+
+  return result;
+}
